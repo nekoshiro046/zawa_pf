d6d90d0eb2ca8fc660e05c0d14564702f3e826ee
nekoshiro046 <nekoshiro046@gmail.com>
2019-12-12 20:12:20 +0900

    first commit

---
+++ js/sistem.js
@@ -0,0 +1,78 @@
+Pace.on('done', function(){
+	$('.wrapper').fadeIn();
+	imgLoad();
+});
+$(window).resize(function(){
+	imgLoad();
+}); 
+function imgLoad(){
+	var container1 = document.querySelector('#container1');
+	var container2 = document.querySelector('#container2');
+	var container3 = document.querySelector('#container3');
+	var $container1 = $('#container1');
+	var $container2 = $('#container2');
+	var $container3 = $('#container3');
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	imagesLoaded(container1, function () {
+	    var msnry = new Masonry(container1, {
+	        itemSelector: '.item', //コンテンツのclass名
+	        fitWidth: true,
+	        initLayout: true,
+	        // columnWidth: 0, //カラムの幅を設定
+	        // columnHeight: 0 //カラムの高さを設定
+	        columnWidth: '.item'
+	    });
+	    msnry.layout();
+	});
+	imagesLoaded(container2, function () {
+	    var msnry = new Masonry(container2, {
+	        itemSelector: '.item',
+	        fitWidth: true,
+	        initLayout: true,
+	        // columnWidth: 0,
+	        // columnHeight: 0
+	        columnWidth: '.item'
+	    });
+	    msnry.layout();
+	});
+	imagesLoaded(container3, function () {
+	    var msnry = new Masonry(container3, {
+	        itemSelector: '.item',
+	        fitWidth: true,
+	        initLayout: true,
+	        // columnWidth: 0,
+	        // columnHeight: 0
+	        columnWidth: '.item'
+	    });
+	    msnry.layout();
+	});
+}
+$(function() {
+	$("#menu").css("display","none");
+   	$("#menu-bt").on("click", function() {
+      $("#menu").slideToggle(300);
+      $(this).toggleClass("active");
+   });
+
+	$('html,body').animate({ scrollTop: 0 }, '1');
+	jQuery(window).scroll(function (){
+        jQuery('.underline').each(function(){
+            var elemPos = jQuery(this).offset().top;
+            var scroll = jQuery(window).scrollTop();
+            var windowHeight = jQuery(window).height();
+            if (scroll > elemPos - windowHeight + 225){
+                jQuery(this).addClass('scrollin');
+            }
+        });
+
+        jQuery('.fadein').each(function(){
+            var elPos = jQuery(this).offset().top;
+            var scroll = jQuery(window).scrollTop();
+            var windowHeight = jQuery(window).height();
+            if (scroll > elPos - windowHeight + 250){
+                jQuery(this).addClass('scrollin');
+            }
+        });
+    });
+});
